9f1fc8e35f480a4f1a431ca3a38c698aa08de233
Beka0n <92734472+Beka0n@users.noreply.github.com>
2022-06-22 16:36:59 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -5,80 +5,15 @@
 ```sh
 waw add ngx-switch
 ```
+
 ## Usage
-
-```javascript
-import { AppComponent } from './app.component';
-import { SwitchModule } from 'ngx-switch';
-
-@NgModule({
-  imports: [SwitchModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
-})
-export class AppModule {
-}
+```html
+<wswitch label="Sound Notification" [(model)]="value.switch" (modelChange)="test()"></wswitch>
 ```
 
-```html
-<wswitch></wswitch>
+## Arguments
 ```
-### Size
-
-> type: *string*
-> default: medium
-```html
-<wswitch size="small"></wswitch>
-<wswitch size="large"></wswitch>
-```
-### Disabled
-
-> type: *boolean*
-> default: false
-```html
-<wswitch disabled></wswitch>
-<wswitch checked [disabled]="true"></wswitch>
-```
-### Color
-
-> type: *string*
-> default: rgb(100, 189, 99)
-```html
-<wswitch color="red"></wswitch>
-```
-
-### Switch Color
-
-> type: *string*
-> default: #fff
-
-```html
-<wswitch switchColor="#fcfcfc"></wswitch>
-```
-
-### Background Color
-
-*Default background color*
-
-> type: *string*
-> default: #fff
-
-```html
-<wswitch defaultBgColor="red"></wswitch>
-```
-
-### Border Color
-
-*Default border color*
-
-> type: *string*
-> default: #dfdfdf
-
-```html
-<wswitch defaultBoColor="black"></wswitch>
-```
-### Toggle text
-
-```html
-<wswitch labelOn="Yes" labelOff="No"></wswitch>
+label: string | Displayed text
+model: boolean | Returns the state of switch
+modelChange | Called when model changes
 ```
--- switch.component.ts
+++ switch.component.ts
@@ -1,85 +1,13 @@
-import { Component, Input, Output, EventEmitter, HostListener } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
 	selector: 'wswitch',
 	templateUrl: './switch.component.html',
 	styleUrls: ['./switch.component.scss']
 })
 export class SwitchComponent{
-	private _checked: boolean;
-	private _disabled: boolean;
-	private _reverse: boolean;
-
-	@Input() wngModel;
+	@Input() model;
 	@Input() label;
-	@Output() wngModelChange = new EventEmitter()
-	@Input() size = 'medium';
-	@Output() change = new EventEmitter<boolean>();
-	@Input() color = 'rgb(47, 207, 45)';
-	@Input() switchOffColor = '';
-	@Input() switchColor = '#fff';
-	@Input() defaultBgColor = '#fff';
-	@Input() defaultBoColor = '#dfdfdf';
-	@Input() labelOn = '';
-	@Input() labelOff = '';
-	@Input() set checked(v: boolean) {
-		this._checked = v !== false;
-	}
-	get checked() {
-		return this._checked;
-	}
-	@Input() set disabled(v: boolean) {
-		this._disabled = v !== false;
-	}
-	get disabled() {
-		return this._disabled;
-	}
-	@Input() set reverse(v: boolean) {
-		this._reverse = v !== false;
-	}
-	get reverse() {
-		return this._reverse;
-	}
-	getColor(flag = '') {
-		if (flag === 'borderColor') {
-			return this.defaultBoColor;
-		}
-		if (flag === 'switchColor') {
-			if (this.reverse) {
-				return !this.checked ? this.switchColor : this.switchOffColor || this.switchColor;
-			}
-			return this.checked ? this.switchColor : this.switchOffColor || this.switchColor;
-		}
-		if (this.reverse) {
-			return !this.checked ? this.color : this.defaultBgColor;
-		}
-		return this.checked ? this.color : this.defaultBgColor;
-	}
-	@HostListener('click')
-	onToggle() {
-		if (this.disabled) {
-			return;
-		}
-		this.checked = !this.checked;
-		this.change.emit(this.checked);
-		this.onChangeCallback(this.checked);
-		this.onTouchedCallback(this.checked);
-	}
-	writeValue(obj: any): void {
-		if (obj !== this.checked) {
-			this.checked = !!obj;
-	  	}
-	}
-	registerOnChange(fn: any) {
-		this.onChangeCallback = fn;
-	}
-	registerOnTouched(fn: any) {
-		this.onTouchedCallback = fn;
-	}
-	setDisabledState(isDisabled: boolean) {
-		this.disabled = isDisabled;
-	}
-	private onTouchedCallback = (v: any) => { };
-	private onChangeCallback = (v: any) => { };
-  
+	@Output() modelChange = new EventEmitter()
+
 	constructor() {}
 }

dbe19a5473e894d6398b1da71fd2a521b206319f
Vlad <58708136+rooben4ik@users.noreply.github.com>
2022-06-28 14:05:19 +0300

    Add files via upload

--- switch.component.ts
+++ switch.component.ts
@@ -5,9 +5,26 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 	styleUrls: ['./switch.component.scss']
 })
 export class SwitchComponent{
-	@Input() model;
+	public _id: string
+	public _disabled : boolean
+	public _checked: boolean = false;
 	@Input() label;
-	@Output() modelChange = new EventEmitter()
+	@Input() required: boolean
+	@Input() name: string
+	@Input() model = false;
 
+	@Input() set checked(v: boolean) {
+		this._checked = v !== false;
+	}
+	@Input() set disabled(v: boolean) {
+		this._disabled = v !== false;
+	}
+	@Output() modelChange = new EventEmitter()		
+	get id() {
+		return this._id;
+	}
+	get disabled() {
+		return this._disabled;
+	}
 	constructor() {}
 }

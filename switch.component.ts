91b305dc786a6a28c47fef1c58e38a2efeecc3a0
Dmytro Maksiuta <40692184+dmaksiuta@users.noreply.github.com>
2022-07-03 13:13:17 +0300

    Update switch.component.ts

--- switch.component.ts
+++ switch.component.ts
@@ -5,31 +5,12 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 	styleUrls: ['./switch.component.scss']
 })
 export class SwitchComponent{
-	private _id: string
-	@Input() set id(s: string) {
-		this._id = s;
-	}
-	get id() {
-		return this._id;
-	}
-	@Input() name !: string; 
+	@Input() id;
+	@Input() name;
 	@Input() wngModel;
 	@Input() label;
+	@Input() disabled: boolean
 	@Output() wngModelChange = new EventEmitter()
-	@Input() required: boolean
-	public _checked: boolean = false;
-	@Input() set checked(v: boolean) {
-		this._checked = v !== false;
-	}
-	get checked() {
-		return this._checked;
-	}	
-	private _disabled: boolean;
-	@Input() set disabled(v: boolean) {
-		this._disabled = v !== false;
-	}
-	get disabled() {
-		return this._disabled;
-	}
 	constructor() {}
 }
+
